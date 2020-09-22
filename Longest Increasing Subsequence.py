10f727691e79b8f974ae2c64b3600a647c65a968
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-22 19:37:06 +0800

    Add files via upload

---
+++ Longest Increasing Subsequence.py
@@ -0,0 +1,18 @@
+#Longest Increasing Subsequence
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums: return 0
+        dp = [0] * len(nums)
+        dp[0] = 1
+        for i in range(1, len(nums)):
+            tmax = 1
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    tmax = max(tmax, dp[j] + 1)
+            dp[i] = tmax
+        return max(dp)
